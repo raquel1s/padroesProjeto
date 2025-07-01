0c9f5c18d33e0d21f3339c3457feb54062326dca
RAQUEL DA SILVA <raquel_silva10@estudante.sesisenai.org.br>
2025-06-30 21:08:33 -0300

    respondendo as perguntas sobre as situações

--- src/situacao1Adapter/Main.java
+++ src/situacao1Adapter/Main.java
@@ -4,9 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+//        Situação 1: Integração de sistemas legados (Adapter)
+//        Você precisa integrar um sistema de pagamento antigo com uma nova API moderna.
+//        As interfaces são diferentes. Como você faria para reaproveitar o código antigo
+//        sem alterar sua estrutura?
+//        Pra reaproveitar o sistema antigo sem mudar ele, eu criei um adaptador
+//        (PagamentoAdaptador) que faz a ponte entre o código novo e o antigo.
+//        Assim, mesmo com interfaces diferentes, consigo usar o sistema antigo
+//        integrando com o novo, sem precisar mexer na estrutura.
+
         PagamentoAdaptador pagamento = new PagamentoAdaptador();
 
         pagamento.pagar(3094.90);
-
     }
 }
--- src/situacao2Decorator/Main.java
+++ src/situacao2Decorator/Main.java
@@ -3,6 +3,15 @@ package situacao2Decorator;
 public class Main {
 
     public static void main(String[] args) {
+
+//        Situação 2: Customização de relatórios (Decorator)
+//        Usuários do sistema podem escolher gerar relatórios com ou sem cabeçalho, rodapé ou
+//        assinatura. Como permitir essa flexibilidade sem criar várias classes diferentes para
+//        cada combinação?
+//        Eu usei o padrão Decorator pra ir adicionando partes no relatório (como cabeçalho, rodapé
+//        e assinatura) sem precisar criar uma classe pra cada combinação. Assim fica flexível e
+//        fácil de montar o relatório do jeito que quiser.
+
         Relatorio relatorio = new RelatorioSimples();
 
         relatorio = new Cabecalho(relatorio);
